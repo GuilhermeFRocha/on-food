451ea9f405081fe05cab9790caab290da4379cf8
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-04-18 21:19:31 -0300

    estruturando projeto

--- components/dishe-details/dishe-details.tsx
+++ components/dishe-details/dishe-details.tsx
@@ -2,14 +2,15 @@ import Styles from "./dishe-details.module.css"
 import { DisheDetailsProps } from "./types";
 import { IconDelivery, IconCategory, IconReviews } from "@/icons";
 
-export function DisheDetails(props:DisheDetailsProps) {
+export function DisheDetails({type, subtitle, title}:DisheDetailsProps) {
+  const Icon = type === "delivery" ? IconDelivery : type === "category" ? IconCategory:IconReviews
   return (
     <div className={Styles.details}>
       <div className={Styles.icon}>
-        Icon
+        <Icon/>
       </div>
-      {props.subtitle && <span>{props.subtitle}</span>}
-      {props.title}
+      {subtitle && <span>{subtitle}</span>}
+      {title}
     </div>
   )
 }
