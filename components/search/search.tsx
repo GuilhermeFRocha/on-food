6b98ea1bd72fec0fafe4da1dec7970ad208678c6
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-04-18 22:32:25 -0300

    estruturando projeto

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,7 +1,25 @@
+import {Menu} from "@headlessui/react"
+import Styles from "./search.module.css";
+
 export function Search() {
   return (
-    <div className="bg-gray-200 rounded-xl">
-      <p>Selecione uma cidade para iniciar</p>
-    </div>
+    <Menu as="div" className={Styles.search}> 
+      <Menu.Button>
+        Selecione uma cidade para iniciar
+      </Menu.Button>
+      <Menu.Items>
+        <div>
+          <Menu.Item>
+            <span>Sao Paulo, SP</span>
+          </Menu.Item>
+          <Menu.Item>
+            <span>Florianópolis, SC</span>
+          </Menu.Item>
+          <Menu.Item>
+            <span>Porto Alegre, RS</span>
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+    </Menu>
   );
 }
